04de67f29711f64bc672bf0f625f5f536e72b1eb
Еж <easzyXD@gmail.com>
2023-05-23 21:17:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     isTg && (
       <BrowserRouter>
         <Header cart={cart} setCart={setCart} setAmountsInCart={setAmountsInCart} />
-        <div className='bg-black'>{tg.platform}</div>
+        <div className='bg-black text-white'>{tg.platform}</div>
         <Routes>
           <Route path='/' exact element={<Main loading={loading} setLoading={setLoading} />} />
           <Route path='/product/:id' element={<Product loading={loading} setLoading={setLoading} setCart={setCart} cart={cart} ammountInCart={ammountInCart} setAmountsInCart={setAmountsInCart} />} />
