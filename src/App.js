7349b706c443693240f1fd793e881ff4961674c0
Еж <easzyXD@gmail.com>
2023-05-23 21:24:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
 
   const [ammountInCart, setAmountsInCart] = useState(0);
 
-  const isTg=true
+  const [isTg, setTg] = useState(false)
 
 
   const tg = window.Telegram.WebApp
 
   if (tg.platform !== "unknown") {
-    
+      setTg(true)
   }
 
   return (
