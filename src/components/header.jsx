e941c1d44309dbef15102bb6b2dce776f8b601d8
Еж <easzyXD@gmail.com>
2023-06-01 23:10:25 +0300

    first commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -183,7 +183,7 @@ const ModalWindow = ({ cart, setCart, setAmountsInCart }) => {
                                 <input placeholder="Адрес / место доставки - " value={place} className="bg-fifth placeholder:text-white p-5" type="text" onChange={onChangePlace} />
                                 <button className="ml-5" onClick={() => { setDeliv(false) }}>Выбрать из существующих</button>
                             </div>}
-                            <Field className="p-5 bg-fifth placeholder:text-white" type="time" placeholder={"время"} name={"time"} />
+                            <span className="text-xl text-white">Время: </span><Field className="p-5 bg-fifth placeholder:text-white" type="time" placeholder={"время"} name={"time"} />
 
                         </div>
                         <div>
