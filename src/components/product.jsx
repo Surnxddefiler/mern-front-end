cbeac70ab246443551fdd05bc089a506e6333682
Еж <easzyXD@gmail.com>
2023-06-02 23:49:47 +0300

    first commit

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -79,7 +79,7 @@ export const Product = ({ setCart, cart, ammountInCart, setAmountsInCart, loadin
                                     </div>
                                     <div className="text-secondary">{filterName[1]} - {obj.nicotine}</div>
                                     <div className="text-fourth">{filterName[1]==="Сопротивление" ? "" : `${filterName[0]} - ${obj.mark}`}</div>
-                                    <div className="text-fifth" >стоимость - {obj.cost} ₴</div>
+                                    <div className="text-fifth" >Стоимость - {obj.cost} ₴</div>
 
                                 </div>
                             )
