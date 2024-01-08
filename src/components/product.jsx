d615d4f84d1a41ad3908e43bef90d5bd41221bcb
Еж <easzyXD@gmail.com>
2024-01-08 20:52:28 +0200

    first commit

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -29,8 +29,8 @@ export const Product = ({ setCart, cart, ammountInCart, setAmountsInCart, loadin
         <>
             {loading ? "" :
                 <div className=" text-white mt-5">
-                    <div className="bg-primary py-5  flex justify-between w-full  rounded-3xl">
-                        <select onChange={(e) => { setMark(e.target.value) }} className="p-2 bg-fifth rounded-2xl" name="" id="">
+                    <div className="bg-primary py-5  flex justify-between w-full gap-4  rounded-3x flex-wrap px-4">
+                        <select onChange={(e) => { setMark(e.target.value) }} className="w-full p-2 bg-fifth rounded-2xl" name="" id="">
                             <option value="" className="bg-fifth text-black" hidden key={"mark-hidden"} >{filterName[0]}</option>
                             <option value="" className="bg-fifth text-black" key={"mark-none"} >-</option>
                             {[...new Set(data.map((obj) => obj.mark))].map((mark, i) => (
@@ -39,7 +39,7 @@ export const Product = ({ setCart, cart, ammountInCart, setAmountsInCart, loadin
                                 </option>
                             ))}
                         </select>
-                        <select onChange={(e) => { setTyagi(e.target.value) }} className="bg-fifth p-2 rounded-2xl" name="" id="">
+                        <select onChange={(e) => { setTyagi(e.target.value) }} className="w-full bg-fifth p-2 rounded-2xl" name="" id="">
                             <option value="" className="bg-fifth text-black" hidden>{filterName[1]}</option>
                             <option value="" className="bg-fifth text-black" >-</option>
                             {[...new Set(data.map((obj) => obj.nicotine))].map((nicotine, i) => (
