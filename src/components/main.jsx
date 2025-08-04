9b295d003cb41dcaab5f0a24b314cf50ea92fdea
Еж <easzyXD@gmail.com>
2025-08-04 22:51:50 +0300

    first

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import {NavLink } from 'react-router-dom'
 
 import '../App.css'
-export const Main = ({loading, setLoading}) => {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        setLoading(true)
-        fetch('https://mernnode-production-873d.up.railway.app/api/nicotine/').then(res => res.json()).then(data => {
-            setData(data.data)
-            setLoading(false)
-        })
-        //  fetch('https://primary-production-23e2.up.railway.app/wp-json/wp/v2/odnorazka?_embed').then(res => res.json()).then(data => {
-        //   console.log(data)
-        // })
-    }, [setLoading])
+export const Main = () => {
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     setLoading(true)
+    //     fetch('https://mernnode-production-873d.up.railway.app/api/nicotine/').then(res => res.json()).then(data => {
+    //         setData(data.data)
+    //         setLoading(false)
+    //     })
+    //     //  fetch('https://primary-production-23e2.up.railway.app/wp-json/wp/v2/odnorazka?_embed').then(res => res.json()).then(data => {
+    //     //   console.log(data)
+    //     // })
+    // }, [setLoading])
     return (
         <>
         
-        {loading ? <img className='absolute translate-x-2/4 loading__animation' style={{top: "150px", right: "50%"}} src="/load.gif" alt="" /> : 
+      
         <div className='flex justify-center flex-wrap gap-8 pt-6'>
             {/* {data.map((obj) => {
                 return (
@@ -45,7 +45,7 @@ export const Main = ({loading, setLoading}) => {
                             <button className='rounded-3xl button py-10'><span unselectable='on' className='rounded-3xl select-none'>ВСЁ ДЛЯ КАЛЬЯНА</span><i></i> </button>
                     </NavLink>
         </div>
-        }
+        
         </>
     )
 }
